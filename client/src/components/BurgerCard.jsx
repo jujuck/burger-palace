9dfe87b963aa67757b773019a22eaf40e5807371
Julien RICHARD <julien.y.richard@gmail.com>
2024-05-17 16:35:14 +0200

    Fin de la card Burger

--- client/src/components/BurgerCard.jsx
+++ client/src/components/BurgerCard.jsx
@@ -1,7 +1,13 @@
+import burgerImage from "../assets/images/burger.jpg";
+
+import "./BurgerCard.css";
+
 function BurgerCard({ burger }) {
   return (
-    <div>
+    <div className="burger-card">
+      <img src={burgerImage} alt={burger.name} />
       <h2>{burger.name}</h2>
+      <p>{burger.description}</p>
     </div>
   );
 }
