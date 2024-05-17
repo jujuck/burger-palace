dbe912fc55f20a7b4aae95a04243d6cb1dd4a585
Julien RICHARD <julien.y.richard@gmail.com>
2024-05-17 16:17:44 +0200

    Map sur les burgers et affichage du titre avec un peu de css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,17 @@ function App() {
     console.log("Effect");
   }, []); */
   const burgers = useLoaderData();
-  console.info(burgers);
 
   return (
     <main className="container">
       <h1>My Burger Palace</h1>
+      <section>
+        {burgers.map((burger) => (
+          <div key={burger.id}>
+            <h2>{burger.name}</h2>
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
